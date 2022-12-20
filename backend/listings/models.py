8bd7e3144a3e6f715026e32d246dc370a133a595
geoafrikana <geoafrikana@gmail.com>
2022-12-20 19:49:11 +0100

    setting up backend and api

---
+++ backend/listings/models.py
@@ -0,0 +1,44 @@
+from django.contrib.gis.db import models
+from django.utils import timezone
+from django.contrib.gis.geos import Point
+
+
+class Listing(models.Model):
+    choices_area = (
+        ('Inner London', 'Inner London'),
+        ('Outer London', 'Outer London')
+    )
+    choices_listing_type = (
+        ('House', 'House'),
+        ('Apartment', 'Apartment'),
+        ('Office', 'Office'),
+    )
+    choices_property_status = (
+        ('Sale', 'Sale'),
+        ('Rent', 'Rent'),
+    )
+
+    choices_rental_frequency = (
+        ('Month', 'Month'),
+        ('Week', 'Week'),
+        ('Day', 'Day'),
+    )
+    title = models.CharField(max_length=150)
+    description= models.TextField(null=True, blank=True)
+    area = models.CharField(max_length=20, blank=True, null=True, choices=choices_area)
+    borough = models.CharField(max_length=50, blank=True, null=True)
+    listing_type = models.CharField(max_length=20, choices=choices_listing_type)
+    property_status = models.CharField(max_length=20, blank=True, null=True, choices=choices_property_status)
+    price = models.DecimalField(max_digits=50, decimal_places=0)
+    rental_frequency = models.CharField(max_length=20, blank=True, null=True, choices=choices_rental_frequency)
+    rooms = models.IntegerField(blank=True, null=True)
+    furnished = models.BooleanField(default=False)
+    pool = models.BooleanField(default=False)
+    elevator = models.BooleanField(default=False)
+    cctv = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+    date_posted= models.DateTimeField(default=timezone.now)
+    location = models.PointField(blank=True, null=True, srid=4326)
+    
+    def __str__(self) -> str:
+        return self.title
